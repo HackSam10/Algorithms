cfb7650a1bd6453650f0cc65b40ce54ae2254342
Samuel <araujosamuel200@gmail.com>
2024-04-30 11:31:12 -0300

    30/04/2024 MergeSort

--- algorithms/MergeSort/MergeSort.c
+++ algorithms/MergeSort/MergeSort.c
@@ -6,32 +6,41 @@
 
 //Function prototypes
 
-int arg_check(int argc);
+int arg_check(int argc); //check argc
 
-
+int *fill_array(char **input, int NumArgs); //Validates and converts input user
 
 
 
 int main(int argc, char *argv[]){
 
-int NumArgs = 0;
+int NumArgs = 0, *array = NULL, index = 0;
 
 
 NumArgs = arg_check(argc);
+array = fill_array(argv + 2, NumArgs); 
 
-//alocar memoria
-//fazer parsing
 //verificar a flag
 
 //deselvolver o algoritmo
 
-printf("%d\n", NumArgs);
+
+for(index = 0; index < NumArgs; index++){
+printf("%d ", array[index]);
+}
+
+putchar(10);
+free(array);
 return 0;
 }
 
 
 
 
+
+
+
+//check argc
 int arg_check(int argc){
 
 int NumArg = 0;    
@@ -43,3 +52,36 @@ exit(1);
 
 return NumArg = argc - 2;
 }
+
+
+
+int *fill_array(char **input, int NumArgs){
+
+int *buffer = NULL, index = 0, status = 0;
+char reg_exp[] = "^[0-9]{1,9}$";
+regex_t cmp = {0};
+
+if(!(buffer = (int *)calloc(NumArgs, sizeof(int)))){
+puts("Error Allocating Memory");
+exit(1);
+    }
+
+regcomp(&cmp, reg_exp, REG_EXTENDED);
+
+while(index < NumArgs){
+    if(status = regexec(&cmp, input[index], 0, NULL, 0)){
+    puts("Invalid Arguments");
+    free(buffer);
+    regfree(&cmp);
+    exit(1);
+        }
+buffer[index] = atoi(input[index]);
+index++;
+    }
+
+regfree(&cmp);
+return buffer;
+}
+
+
+
